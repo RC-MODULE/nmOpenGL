62b602e37d578f8c3b194df65dcef3b3f20e97d9
Ivan Zhilenkov <i.zhilenkov@module.ru>
2019-10-10 12:49:30 +0300

    fixed model NeuroMatrix

--- models/triangulisation/triangulisation.cpp
+++ models/triangulisation/triangulisation.cpp
@@ -59,54 +59,65 @@ bool read_trn_from_str(const vector<string>& input, Triangle& trn2splt, float sc
 	trn2splt.a.set(stof(A[0], 0), stof(A[1], 0), stof(A[2], 0));
 	trn2splt.b.set(stof(B[0], 0), stof(B[1], 0), stof(B[2], 0));
 	trn2splt.c.set(stof(C[0], 0), stof(C[1], 0), stof(C[2], 0));
-	trn2splt.a = trn2splt.a * scale;
-	trn2splt.b = trn2splt.b * scale;
-	trn2splt.c = trn2splt.c * scale;
+	trn2splt.a.scal(scale);
+	trn2splt.b.scal(scale);
+	trn2splt.c.scal(scale);
 
-	return trn2splt.isFitIntoRectangle(MAX_DIFF, MAX_DIFF);
+	return trn2splt.checkLengths(MAX_DIFF);
 }
 
-void split(Triangle input, vector<Triangle>& output) {
+void split(Triangle &input, vector<Triangle>& output) {
 	///test
 	//cout << "split was called" << endl;
 	Triangle trn1(input.a, input.b, input.c);
 	Triangle trn2;
-	if()
-	if (!trn1.isFitIntoRectangle(MAX_DIFF, MAX_DIFF)) {
+	trn1.split(trn2);
+	if (!trn1.checkLengths(MAX_DIFF)) {
 		split(trn1, output);
 	}
 	else {
 		output.push_back(trn1);
 	}
-/*	input.sort_by_length();
-	//input.print_trn();
-	for (int i = 0; i<3; i++) {
-		trn1.A[i] = input.A[i];
-		trn1.C[i] = input.C[i];
-		trn1.B[i] = input.mdl_pnt_AB[i];
-
-		trn2.A[i] = input.mdl_pnt_AB[i];
-		trn2.B[i] = input.B[i];
-		trn2.C[i] = input.C[i];
-	}
-	//cout << "trn1" << endl;
-	//trn1.print_trn();
-	//cout << "trn2" << endl;
-	//trn2.print_trn();
-
-	if (trn1.sort_by_length()) {
-		split(trn1, output);
-	}
-	else {
-		output.push_back(trn1);
-	}
-	if (trn2.sort_by_length()) {
+	if (!trn2.checkLengths(MAX_DIFF)) {
 		split(trn2, output);
 	}
 	else {
 		output.push_back(trn2);
-	}*/
+	}
 	return;
+};
+
+void write_1_trn_into_file(ofstream& output, const Triangle& trn2wrt) {
+	output << "      vertex ";
+	output << trn2wrt.a.x << ' ' << trn2wrt.a.y << ' ' << trn2wrt.a.z << endl;
+	output << "      vertex ";
+	output << trn2wrt.b.x << ' ' << trn2wrt.b.y << ' ' << trn2wrt.b.z << endl;
+	output << "      vertex ";
+	output << trn2wrt.c.x << ' ' << trn2wrt.c.y << ' ' << trn2wrt.c.z << endl;
+};
+
+void write_splited_trn(ofstream& output, const vector<Triangle>& trn2wrt, const string& normal) {
+	for (Triangle item : trn2wrt) {
+		output << normal << endl;
+		output << "    outer loop" << endl;
+		write_1_trn_into_file(output, item);
+		output << "    endloop" << endl;
+		output << "  endfacet" << endl;
+	}
+};
+
+void write_scaled_trn(ofstream& output, const Triangle &trn2wrt, const string& normal) {
+	output << normal << endl;
+	output << "    outer loop" << endl;
+	write_1_trn_into_file(output, trn2wrt);
+	output << "    endloop" << endl;
+	output << "  endfacet" << endl;
+};
+
+void write_saved_trn(ofstream& output, const vector<string>& trn2save) {
+	for (int i = 0; i<7; i++) {
+		output << trn2save[i] << endl;
+	}
 };
 
 
@@ -118,6 +129,11 @@ int main(int argc, char *argv[]){
 		return 0xBAD00BAD;
 	}*/
 	float scale;
+
+	//path = "d:\\GIT\\nmOpenGL\\models\\NM.stl";
+	//path2wrt = "d:\\GIT\\nmOpenGL\\models\\NM_32.stl";
+	//scale = 1.5;
+
 	cout << "WELCOME TO THE TRIANGULISATION PROGRAM!!!!!" << endl;
 	cout << "Please enter the path to file for transformation" << endl;
 	cin >> path;
@@ -145,303 +161,17 @@ int main(int argc, char *argv[]){
 		if (!input) {
 			break;
 		}
-		if (!read_trn_from_str(saveTrian, splitTrian, scale)) {
+		if (read_trn_from_str(saveTrian, splitTrian, scale)) {
+			write_scaled_trn(output, splitTrian, saveTrian[0]);
+		}else {
 			writeTrian.clear();
 			split(splitTrian, writeTrian);
-			//write_splited_trn(output, trn2wrt, trn2save[0]);
-		}else {
-			//write_saved_trn(output,trn2save);
-			//write_scaled_trn(output, trn2splt, trn2save[0]);
+			write_splited_trn(output, writeTrian, saveTrian[0]);
 		}
-	}
-	/*triangle trn2splt;
-	vector<triangle> trn2wrt;
-	vector<string> trn2save;
-	int i = 0;
-	while (input) {
-		trn2save.clear();
-		save_trn(input, trn2save);
-		if (!input) {
-			break;
-		}
-		if (read_trn_from_str(trn2save, trn2splt, scale)) {
-			trn2wrt.clear();
-			split(trn2splt, trn2wrt);
-			write_splited_trn(output, trn2wrt, trn2save[0]);
-		}
-		else {
-			//write_saved_trn(output,trn2save);
-			write_scaled_trn(output, trn2splt, trn2save[0]);
-		}
-		//cout << i << endl;
-		//i++;
 	}
 	output << "endsolid OpenSCAD_Model";
 	output.close();
 	input.close();
-	cout << "ALL PROCESSING HAS BEEN DONE ENJOY THE NEW STL!!!" << endl;*/
-}
-/*class triangle{
-public:
-	float A[3];
-	float B[3];
-	float C[3];
-	float mdl_pnt_AB[3];
+	cout << "ALL PROCESSING HAS BEEN DONE ENJOY THE NEW STL!!!" << endl; 
 	
-	bool sort_by_length(){
-		//the perpose of the method is to sort sides of triangle that way that the longest side would be AB
-		float length_AB = (A[0]-B[0])*(A[0]-B[0]) + (A[1]-B[1])*(A[1]-B[1]) + (A[2]-B[2])*(A[2]-B[2]) ;
-		float length_AC = (A[0]-C[0])*(A[0]-C[0]) + (A[1]-C[1])*(A[1]-C[1]) + (A[2]-C[2])*(A[2]-C[2]) ; 
-		float length_BC = (B[0]-C[0])*(B[0]-C[0]) + (B[1]-C[1])*(B[1]-C[1]) + (B[2]-C[2])*(B[2]-C[2]) ;
-			
-		vector<float> v2sort = {length_AB,length_AC,length_BC};
-		sort(v2sort.begin(), v2sort.end());
-		if(length_AB == v2sort[2]){
-			//AB side is the greatest side no action needed
-		}else{
-			if(length_AC == v2sort[2]){
-				for(int i=0;i<3;i++){
-					swap(C[i],B[i]);
-				}
-			}else{
-				for(int i=0;i<3;i++){
-					swap(A[i],B[i]);
-					swap(B[i],C[i]);
-				}
-			}
-		}
-		//set mdl point
-		for(int i=0;i<3;i++){
-			mdl_pnt_AB[i] = A[i] + (B[i] - A[i])*0.5;	
-		}
-		if(v2sort[2] > MAX_DIFF*MAX_DIFF){
-			return true;
-		}else{
-            return false;
-		}
-	}
-	void print_trn(){
-		cout << "A ";
-		for(int i=0;i<3;i++){
-			cout << A[i] << " ";
-		}
-		cout << endl;
-		cout << "B ";
-		for(int i=0;i<3;i++){
-			cout << B[i] << " ";
-		}
-		cout << endl;
-		cout << "C ";
-		for(int i=0;i<3;i++){
-			cout << B[i] << " ";
-		}
-		cout << endl;
-	}
-};
-
-bool read_trn_from_str(const vector<string>& input, triangle& trn2splt,float scale){
-	string A[3];
-	string B[3];
-	string C[3];
-	string buff[3];
-	////get rid of " " before verex word
-	for(int j=0;j<3;j++){
-		for(int i=6;i<input[2+j].size();i++){
-			buff[j] += input[2+j][i];
-		}
-	}
-	string buff_2[3];	
-	for(int j=0;j<3;j++){
-		for(int i=6;i<buff[j].size();i++){
-			buff_2[j] += buff[j][i];
-		}
-	}
-	////
-	for(int i=0,j=-1;i<buff_2[0].size();i++){
-		if(buff_2[0][i] == ' '){
-			j++;
-		}else A[j] += buff_2[0][i];
-	}
-	for(int i=0,j=-1;i<buff_2[1].size();i++){
-		if(buff_2[1][i] == ' '){
-			j++;
-		}else B[j] += buff_2[1][i];
-	}
-	for(int i=0,j=-1;i<buff_2[2].size();i++){
-		if(buff_2[2][i] == ' '){
-			j++;
-		}else C[j] += buff_2[2][i];
-	}
-	///convert 
-	for(int i=0;i<3;i++){
-		trn2splt.A[i] = scale*stof(A[i],0);
-	}
-	for(int i=0;i<3;i++){
-		trn2splt.B[i] = scale*stof(B[i],0);
-	}
-	for(int i=0;i<3;i++){
-		trn2splt.C[i] = scale*stof(C[i],0);
-	}
-	return trn2splt.sort_by_length();
 }
-
-bool readTriangle(fstream &input, triangle &trn2splt, float scale){
-	string vertex_pass;
-	string A[3];
-	string B[3];
-	string C[3];
-	getline(input,vertex_pass);
-	input >> vertex_pass;
-	for(int i=0;i<3;i++){
-		input >> A[i];
-		//cout << A[i] << " ";
-	}
-	input >> vertex_pass;
-	cout << endl;
-	for(int i=0;i<3;i++){
-		input >> B[i];
-		//cout << B[i] << " ";
-	}
-	cout << endl;
-	input >> vertex_pass;
-	for(int i=0;i<3;i++){
-		input >> C[i];
-		//cout << C[i] << " ";
-	}
-	///convert 
-	for(int i=0;i<3;i++){
-		trn2splt.A[i] = scale*stof(A[i],0);
-	}
-	for(int i=0;i<3;i++){
-		trn2splt.B[i] = scale*stof(B[i],0);
-	}
-	for(int i=0;i<3;i++){
-		trn2splt.C[i] = scale*stof(C[i],0);
-	}
-	return trn2splt.sort_by_length()	;
-};
-void split(triangle input, vector<triangle>& output){
-    ///test
-    //cout << "split was called" << endl;
-    triangle trn1,trn2;
-    input.sort_by_length();
-    //input.print_trn();
-    for(int i =0;i<3;i++){
-        trn1.A[i] = input.A[i];
-        trn1.C[i] = input.C[i];
-        trn1.B[i] = input.mdl_pnt_AB[i];
-
-        trn2.A[i] = input.mdl_pnt_AB[i];
-        trn2.B[i] = input.B[i];
-        trn2.C[i] = input.C[i];
-    }
-    //cout << "trn1" << endl;
-    //trn1.print_trn();
-    //cout << "trn2" << endl;
-    //trn2.print_trn();
-
-    if(trn1.sort_by_length()){
-        split(trn1,output);
-    }else{
-        output.push_back(trn1);
-    }
-    if(trn2.sort_by_length()){
-        split(trn2,output);
-    }else{
-        output.push_back(trn2);
-    }
-    return;
-};
-void write_1_trn_into_file(ofstream& output, const triangle& trn2wrt){
-	output << "      vertex ";
-	output << trn2wrt.A[0] << ' ' << trn2wrt.A[1] << ' ' << trn2wrt.A[2] << endl;
-	output << "      vertex ";
-	output << trn2wrt.B[0] << ' ' << trn2wrt.B[1] << ' ' << trn2wrt.B[2] << endl;
-	output << "      vertex ";
-	output << trn2wrt.C[0] << ' ' << trn2wrt.C[1] << ' ' << trn2wrt.C[2] << endl;
-};
-
-void write_splited_trn(ofstream& output, const vector<triangle>& trn2wrt, const string& normal){
-	for(triangle item : trn2wrt){
-		output << normal << endl;
-		output << "    outer loop" << endl;
-		write_1_trn_into_file(output,item);
-		output << "    endloop" << endl;
-   	output << "  endfacet" << endl;
-	}
-};
-
-void write_scaled_trn(ofstream& output, const triangle trn2wrt, const string& normal){
-		output << normal << endl;
-		output << "    outer loop" << endl;
-		write_1_trn_into_file(output,trn2wrt);
-		output << "    endloop" << endl;
-   	output << "  endfacet" << endl;
-};
-
-void write_saved_trn(ofstream& output, const vector<string>& trn2save){
-	for(int i=0;i<7;i++){
-		output << trn2save[i] << endl;
-	}
-};
-void save_trn(fstream& input, vector<string>& vec2save){
-	//long position = input.tellp();
-	string buff;
-	for(int i=0;i<7;i++){
-		getline(input,buff);
-		vec2save.push_back(buff);
-	}
-	//input.seekp(position);
-};
-
-
-
-int main()
-{
-  string path;
-	string path2wrt;
-	string buff;
-	float scale;
-  cout << "WELCOME TO THE TRIANGULISATION PROGRAM!!!!!" << endl;
-  cout << "Please enter the path to file for transformation" << endl;
-  cin >> path; 
-  fstream input(path);
-	if(input){
-		getline(input,buff);
-		cout << "file was opened" << endl;
-	}
-	else cout << "file does not exist" << endl;
-  cout << "Please enter the path to file for writting the resault stl " << endl;
-  cin >> path2wrt;
-  cout << "Please enter the scale" << endl;
-  cin >> scale;
-
-	ofstream output(path2wrt);
-	output << buff << endl;
-
-	triangle trn2splt;
-	vector<triangle> trn2wrt;
-	vector<string> trn2save;
-	int i =0;	
-	while(input){
-    trn2save.clear();
-		save_trn(input,trn2save);
-		if(!input){
-			break;
-		}
-		if(read_trn_from_str(trn2save,trn2splt,scale)){
-			trn2wrt.clear();
-			split(trn2splt,trn2wrt);
-			write_splited_trn(output,trn2wrt,trn2save[0]);
-		}else{
-			//write_saved_trn(output,trn2save);
-			write_scaled_trn(output,trn2splt,trn2save[0]);
-		}
-		//cout << i << endl;
-		//i++;
-	}
-	output << "endsolid OpenSCAD_Model";
-	output.close();
-	input.close();
-  cout << "ALL PROCESSING HAS BEEN DONE ENJOY THE NEW STL!!!" << endl;
-}//end of maine*/
--- models/triangulisation/triangulisation.h
+++ models/triangulisation/triangulisation.h
@@ -30,89 +30,28 @@ public:
 		x = pointX;
 		y = pointY;
 	}
-	
-	operator float() { 
-		return sqrt(x*x + y*y + z*z);
-	}
-	
-	Vector3 operator+(Vector3 vector) {
-		Vector3 result;
-		result.x = x + vector.x;
-		result.y = y + vector.y;
-		result.z = z + vector.z;
-		return result;
-	}
 
-	Vector3 operator+(float v) {
-		Vector3 result;
-		result.x = x + v;
-		result.y = y + v;
-		result.z = z + v;
-		return result;
-	}
-	
-	Vector3 operator-(Vector3 vector) {
-		Vector3 result;
-		result.x = x - vector.x;
-		result.y = y - vector.y;
-		result.z = z - vector.z;
-		return result;
-	}
-
-	Vector3 operator-(float v) {
-		Vector3 result;
-		result.x = x - v;
-		result.y = y - v;
-		result.z = z - v;
-		return result;
-	}
-	
-	Vector3 operator*(Vector3 vector) {
-		Vector3 result;
-		result.x = x * vector.x;
-		result.y = y * vector.y;
-		result.z = z * vector.z;
-		return result;
-	}
-
-	Vector3 operator*(float v) {
-		Vector3 result;
-		result.x = x * v;
-		result.y = y * v;
-		result.z = z * v;
-		return result;
-	}
-	
-	Vector3 operator/(Vector3 vector) {
-		Vector3 result;
-		result.x = x / vector.x;
-		result.y = y / vector.y;
-		result.z = z / vector.z;
-		return result;
-	}
-
-	Vector3 operator/(float v) {
-		Vector3 result;
-		result.x = x / v;
-		result.y = y / v;
-		result.z = z / v;
-		return result;
-	}
-
-	Vector3 operator=(Vector3 vector) {
+	Vector3 &operator=(Vector3 &vector) {
 		x = vector.x;
 		y = vector.y;
 		z = vector.z;
 		return *this;
 	}
 
-	Vector3 operator=(float v) {
-		x = v;
-		y = v;
-		z = v;
+	Vector3 &operator-(Vector3 &vector) {
+		Vector3 result;
+		result.x = x - vector.x;
+		result.y = y - vector.y;
+		result.z = z - vector.z;
 		return *this;
 	}
-	
+
+	void scal(float value) {
+		x *= value;
+		y *= value;
+		z *= value;
+	}
+
 	
 };
 
@@ -121,25 +60,57 @@ public:
 	Vector3 a;
 	Vector3 b;
 	Vector3 c;
-
+public:
 	Triangle() {
 	}
 
-	Triangle(Vector3 vertexA, Vector3 vertexB, Vector3 vertexC) {
+	Triangle(Vector3 &vertexA, Vector3 &vertexB, Vector3 &vertexC) {
 		a = vertexA;
 		b = vertexB;
 		c = vertexC;
 	}
 	
-	bool isFitIntoRectangle(int width, int height){
-		Vector3 AC = a - c;
-		Vector3 AB = a - b;
-		Vector3 BC = b - c;
-		if( abs(AC.x) > width  || abs(AB.x) > width  || abs(BC.x) > width ||
-			abs(AC.y) > height || abs(AB.y) > height || abs(BC.y) > height){
+	bool checkLengths(float maxSide){
+		float lengthAB = (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y) + (b.z - a.z) * (b.z - a.z);
+		float lengthAC = (c.x - a.x) * (c.x - a.x) + (c.y - a.y) * (c.y - a.y) + (c.z - a.z) * (c.z - a.z);
+		float lengthBC = (c.x - b.x) * (c.x - b.x) + (c.y - b.y) * (c.y - b.y) + (c.z - b.z) * (c.z - b.z);
+		float squareSide = maxSide * maxSide;
+		if( lengthAB > squareSide || lengthAC > squareSide || lengthBC > squareSide){
 			return false;
 		}
 		return true;
+	}
+
+	void split(Triangle& triangle) {
+		Vector3 middle;
+		float lengthAB = (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y) + (b.z - a.z) * (b.z - a.z);
+		float lengthAC = (c.x - a.x) * (c.x - a.x) + (c.y - a.y) * (c.y - a.y) + (c.z - a.z) * (c.z - a.z);
+		float lengthBC = (c.x - b.x) * (c.x - b.x) + (c.y - b.y) * (c.y - b.y) + (c.z - b.z) * (c.z - b.z);
+		if (lengthAB > lengthAC && lengthAB > lengthBC) {
+			middle.x = a.x + (b.x - a.x) / 2;
+			middle.y = a.y + (b.y - a.y) / 2;
+			middle.z = a.z + (b.z - a.z) / 2;
+			triangle.a = middle;
+			triangle.b = b;
+			triangle.c = c;			
+			b = middle;
+		}else if (lengthAC > lengthAB && lengthAC > lengthBC) {
+			middle.x = a.x + (c.x - a.x) / 2;
+			middle.y = a.y + (c.y - a.y) / 2;
+			middle.z = a.z + (c.z - a.z) / 2;
+			triangle.a = middle;
+			triangle.b = b;
+			triangle.c = c;			
+			c = middle;
+		}else{
+			middle.x = b.x + (c.x - b.x) / 2;
+			middle.y = b.y + (c.y - b.y) / 2;
+			middle.z = b.z + (c.z - b.z) / 2;
+			triangle.a = a;
+			triangle.b = middle;
+			triangle.c = c;
+			c = middle;
+		}
 	}
 };
 
