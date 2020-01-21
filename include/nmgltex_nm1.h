1ff9a0da32c3f225b7173946b676a42dcd6432c6
a.a.zhdanov <a.a.zhdanov@mai.ru>
2020-01-21 15:28:19 +0300

    Добавлена реализация функции nmglActiveTexture
    * Добавлен прототип функции
    * Добавлены поля для реализации nmglActiveTexture в текстурные контексты (и инициализация полей)
    * Добавлена реализация функции для nm0
    * Добавлен код операции для nm1 (SET_ACTIVE_TEXTURE) и реализация операции
    
    Компилируется и выполняется успешно (12101, модель x86) в следующей конфигурации коммитов:
    * hal - 735d7803
    * nmpp - 4dd70b5c1

--- include/nmgltex_nm1.h
+++ include/nmgltex_nm1.h
@@ -3,13 +3,18 @@
 
 struct NMGL_Context_NM1_Texture {
 	
+	/**
+	*  Индекс активного текстурного модуля в массиве текстурных модулей.
+	*  Используется для доступа к активному текстурному модулю
+	*/
+	unsigned int activeTexUnitIndex;
+
 	
 	void init(){
 	
-
+		activeTexUnitIndex = 0;
 	}
 	
 };
-
 
 #endif
