e21aac587215372a2bd4d7e949814878f2528f44
a.a.zhdanov <a.a.zhdanov@mai.ru>
2020-01-21 14:51:10 +0300

    Добавлены структуры контекстов для текстурирования
    Для реализации текстурирования в контексты процессоров nm0 и nm1
    добавлены поля для хранения текстурных контекстов, содержащих значения,
    необходимые для текстурирования. Поля - структуры, объявленные в
    отдельных файлах. Каждый текстурный контекст содержит метод для
    инициализации, который вызывается в соответствующем методе инициализации
    основного контекста (NMGL_Context_NM1 и NMGL_Context_NM0). Текстурные контексты добавлены
    как поля основных контекстов(а не как отдельные переменные) для простоты и универсальности.

---
+++ include/nmgltex_nm0.h
@@ -0,0 +1,14 @@
+#ifndef __NMGLTEX_NM0_H__
+#define __NMGLTEX_NM0_H__
+
+struct NMGL_Context_NM0_Texture {
+	
+	
+	void init(){
+
+	}
+	
+};
+
+
+#endif
---
+++ include/nmgltex_nm1.h
@@ -0,0 +1,15 @@
+#ifndef __NMGLTEX_NM1_H__
+#define __NMGLTEX_NM1_H__
+
+struct NMGL_Context_NM1_Texture {
+	
+	
+	void init(){
+	
+
+	}
+	
+};
+
+
+#endif
