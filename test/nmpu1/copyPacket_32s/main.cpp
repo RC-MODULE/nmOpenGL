3d144ce6d920af223146fdf58732117d14108755
Ivan Zhilenkov <i.zhilenkov@module.ru>
2020-03-23 16:52:53 +0300

    added test mAndVxN_32u

--- test/nmpu1/copyPacket_32s/main.cpp
+++ test/nmpu1/copyPacket_32s/main.cpp
@@ -1,47 +1,34 @@
 #include "nmpp.h"
 #include "stdio.h"
-#include "minrep.h"
 #include "time.h"
-#include "demo3d_common.h"
+#include "demo3d_nm1.h"
 
 ///////////////////////////////////////////////////////////////////////////////////////
-nm64s *L0;
-nm64s *L1;
-nm64s *G0;
-nm64s *G1;
-const int KB=1024/8;
-const int SizeL0=30*KB;
-const int SizeL1=30*KB;
+#define SIZE 16384
+#define COUNT 32
 
-const int SizeG0=30*KB;
-const int SizeG1=30*KB;
-extern "C" long tmp;
-
-
-SECTION(".data_imu1")	int src[16384];
+SECTION(".data_imu1")	int src[SIZE];
 		
-SECTION(".data_imu2") int* ppSrc[32];
-SECTION(".data_imu2") int* ppDst[32];
-SECTION(".data_imu2") int sizes[32];
+SECTION(".data_imu2") int* ppSrc[COUNT];
+SECTION(".data_imu2") int* ppDst[COUNT];
+SECTION(".data_imu2") int sizes[COUNT];
 	
-SECTION(".data_imu3")	int dst[16384];
+SECTION(".data_imu3")	int dst[SIZE];
 
-#define SIZE_ONE 32*32
 
-extern "C" void copyPacket_32s(nm32s** pSrc, nm32s** pDst, int* size, int count);
 
 int main()
 {		
 	unsigned int crc = 0;
 	clock_t t0, t1;
-	nmppsRandUniform_32s(src,16384);
-	nmppsSet_32s(dst,0xCDCDCDCD,16384);
-	for(int i=0;i<32;i++){
+	nmppsRandUniform_32s(src,SIZE);
+	nmppsSet_32s(dst,0xCDCDCDCD,SIZE);
+	for(int i=0;i<COUNT;i++){
 		ppSrc[i] = src;
 		ppDst[i] = dst;
-		sizes[i] = 31;
+		sizes[i] = 16;
 	}
-	copyPacket_32s(ppSrc, ppDst, sizes, 3);
-	nmppsCrcAcc_32s(dst,16384,&crc);
+	copyPacket_32s(ppSrc, ppDst, sizes, COUNT);
+	nmppsCrcAcc_32s(dst,SIZE,&crc);
 	return crc>>2;
 }
