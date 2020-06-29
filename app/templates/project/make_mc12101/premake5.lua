dd78d53d6384ac4ae478cd087754175e318e11b0
Ivan Zhilenkov <i.zhilenkov@module.ru>
2020-06-29 16:43:20 +0300

    added script, which create new project

--- app/templates/project/make_mc12101/premake5.lua
+++ app/templates/project/make_mc12101/premake5.lua
@@ -9,7 +9,7 @@ solution "demo3d-mc12101"
    project "mc12101-x86"
 		kind "ConsoleApp"
 		language "C++"
-		files { "../../include/*.h","../src_host/*.cpp", "../src_common/*.cpp", "../src_host/*.c", "../../nmglvs_mc12101-gcc/src_host/*.cpp"}
+		files { "../../include/*.h","../src_host/*.cpp", "../../nmglvs_mc12101-gcc/src_host/*.cpp"}
 		libdirs { "$(MC12101)/lib","$(VSHELL)/lib","$(NMPP)/lib","$(HAL)/lib"}
 		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../include", "../include"}
 		links { "mc12101load.lib","vshell.lib","nmpp-x86.lib","hal-mc12101-x86d.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
@@ -25,7 +25,7 @@ solution "demo3d-mc12101"
 		 
 	project "mc12101-nmc4-0"
       kind "Makefile"
-      files { "../../include/*.h","../src_target0/*.*", "../../src_proc0/nm/*.*","../../src_proc0/common/*.*","../../src_proc0/nmgl/*.*","../../nmglvs_mc12101-gcc/src_nmc0/*.*", "mc12101brd-nmc0.cfg", "Makefile0" }
+      files { "../../include/*.h","../src_target0/*.*", "../../nmglvs_mc12101-gcc/src_nmc0/*.*", "mc12101brd-nmc0.cfg", "Makefile0" }
 	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include","../include"}
 	 
 	  configuration "Debug"
@@ -40,7 +40,7 @@ solution "demo3d-mc12101"
 		   
 	project "mc12101-nmc4-1"
       kind "Makefile"
-      files { "../../include/*.h","../src_target1/*.*", "../../src_proc1/nm/*.*","../../src_proc1/common/*.*", "../../nmglvs_mc12101-gcc/src_nmc1/*.*","mc12101brd-nmc1.cfg", "Makefile1" }
+      files { "../../include/*.h","../src_target1/*.*", "../../nmglvs_mc12101-gcc/src_nmc1/*.*","mc12101brd-nmc1.cfg", "Makefile1" }
 	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include"}
 	 
 	  configuration "Debug"
--- app/templates/project/src_target0/nm_main0.cpp
+++ app/templates/project/src_target0/nm_main0.cpp
@@ -2,31 +2,25 @@
 #include "nmglvs_nmc0.h"
 
 float triangles[24]{
-	0,0,50, 1,
-	0,30,50, 1,
-	32,30,50, 1,
-
-	-5,0,25, 1,
-	-5,30,25, 1,
-	27,30,25, 1
-
-	/*0,0,500,
-	30,0,500,
-	30,30,500,*/
+	0,0,
+	0,31,
+	31,31,
+	
+	0, 0,
+	30, 0,
+	30, 30,
+	
 };
 
-float color[24]{
+float color[48]{
 	1,0,0, 1,
 	1,0,0, 1,
 	1,0,0, 1,
 
-	0,0,1, 1,
-	0,0,1, 1,
-	0,0,1, 1,
-
-	/*0,0,500,
-	30,0,500,
-	30,30,500,*/
+	1,0,1, 1,
+	1,0,1, 1,
+	1,0,1, 1,
+	
 };
 
 int main()
@@ -39,13 +33,12 @@ int main()
 	nmglOrthof(-WIDTH_IMAGE/2, WIDTH_IMAGE / 2, -HEIGHT_IMAGE / 2, HEIGHT_IMAGE / 2, 0, 100);
 	
 
-	nmglEnable(NMGL_DEPTH_TEST);
-	nmglVertexPointer(4, NMGL_FLOAT, 0, triangles);
+	nmglVertexPointer(2, NMGL_FLOAT, 0, triangles);
 	nmglColorPointer(4, NMGL_FLOAT, 0, color);
 	nmglEnableClientState(NMGL_VERTEX_ARRAY);
 	nmglEnableClientState(NMGL_COLOR_ARRAY);
 	while(nmglvsNm0Run()){
-		nmglClear(NMGL_COLOR_BUFFER_BIT | NMGL_DEPTH_BUFFER_BIT);
+		nmglClear(NMGL_COLOR_BUFFER_BIT);
 
 		nmglDrawArrays(NMGL_TRIANGLES, 0, 6);
 
