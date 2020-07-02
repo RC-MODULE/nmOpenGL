e71c1e12f05ebb4e7813785afd289622752d1f3c
Aleksandr Zhdanov <a.a.zhdanov@mai.ru>
2020-07-02 16:23:25 +0300

    Remove macro NMGL_MAX_TEX_UNITS from nmgltex_nm0.h
    Macro NMGL_MAX_TEX_UNITS is located in nmgltex_common.h so it is
    unnecessary in nmgltex_nm0.h

--- include/nmgltex_nm0.h
+++ include/nmgltex_nm0.h
@@ -1,10 +1,6 @@
 #ifndef __NMGLTEX_NM0_H__
 #define __NMGLTEX_NM0_H__
 
-/*
-* Максимальное поддерживаемое количество текстурных модулей
-*/
-#define NMGL_MAX_TEX_UNITS 1 //TODO: Значение, возвращаемое при вызове функции glGetIntegeri с параметром NMGL_MAX_TEXTURE_UNITS
 
 struct NMGL_Context_NM0_Texture {
 	
