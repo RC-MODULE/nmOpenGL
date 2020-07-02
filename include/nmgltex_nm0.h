0020c13b27b37138e901ca770278466f382f131f
Aleksandr Zhdanov <a.a.zhdanov@mai.ru>
2020-07-02 16:26:51 +0300

    Add nmglTexCoordPointer function
    * nmglTexCoordPointer function was added.
    * Necessary fields were added to the nmpu0 texture context to make
    the function work: texcoord array, client active texture unit
    identifiers.
    * Init function of nmpu0 context was updated accordingly.

--- include/nmgltex_nm0.h
+++ include/nmgltex_nm0.h
@@ -1,8 +1,15 @@
 #ifndef __NMGLTEX_NM0_H__
 #define __NMGLTEX_NM0_H__
 
+#include "demo3d_common.h"
+#include "nmgltex_common.h"
 
 struct NMGL_Context_NM0_Texture {
+	
+	/**
+	* Массивы текстурных координат
+	*/
+	Array texcoordArray[NMGL_MAX_TEX_UNITS];
 	
 	/**
 	*  Имя активного текстурного модуля.
@@ -15,11 +22,33 @@ struct NMGL_Context_NM0_Texture {
 	*/
 	unsigned int activeTexUnitIndex;
 	
+	/**
+	*  Имя клиентского активного текстурного модуля.
+	*/
+	NMGLenum clientActiveTexUnit;
+
+	/**
+	*  Индекс клиентского активного текстурного модуля в массиве текстурных модулей.
+	*  Используется для доступа к клиентскому активному текстурному модулю
+	*/
+	unsigned int clientActiveTexUnitIndex;
+	
 	
 	void init(){
 	
 		activeTexUnit = NMGL_TEXTURE0;
 		activeTexUnitIndex = 0;
+		clientActiveTexUnit = NMGL_TEXTURE0;
+		clientActiveTexUnitIndex = 0;
+		
+		for (int i = 0; i < NMGL_MAX_TEX_UNITS; i++)
+		{
+			texcoordArray[i].pointer = 0;
+			texcoordArray[i].size = 4;
+			texcoordArray[i].stride = 0;
+			texcoordArray[i].type = NMGL_FLOAT;
+			texcoordArray[i].enabled = NMGL_FALSE;
+		}
 	}
 	
 };
