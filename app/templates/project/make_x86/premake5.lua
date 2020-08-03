968973cebe59fb135e5b3c50401d5cc6b6b6967f
Alex Klimenko <a.v.klimenko@mai.ru>
2020-08-03 16:56:43 +0300

    Подправил пути для app/templates/project/make_x86

--- app/templates/project/make_x86/premake5.lua
+++ app/templates/project/make_x86/premake5.lua
@@ -7,9 +7,9 @@ solution "demo3d-host-x86"
    project "demo3d-host-x86"
 		kind "ConsoleApp"
 		language "C++"
-		files { "../../include/*.h","../src_host/*.cpp","../src_host/*.c", "../../nmglvs_mc12101-gcc/src_host/*.cpp"}
+		files { "../../../../include/*.h","../src_host/*.cpp","../src_host/*.c", "../../../../nmglvs_mc12101-gcc/src_host/*.cpp"}
 		libdirs { "$(VSHELL)/lib","$(NMPP)/lib","$(HAL)/lib"}
-		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../include"}
+		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../../../include"}
 		links { "vshell.lib","nmpp-x86.lib","hal-virtual-x86.lib"}
 
       configuration "Debug"
@@ -30,9 +30,9 @@ solution "demo3d-target0-x86"
    project "demo3d-target0-x86"
       kind "ConsoleApp"
 	  language "C++"
-      files { "../../include/*.h","../src_target0/*.*","../../nmglvs_mc12101-gcc/src_nmc0/*.*", "Makefile0" }
-	  libdirs { "$(NMPP)/lib","$(HAL)/lib", "../../lib"}
-	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include"}
+      files { "../../../../include/*.h","../src_target0/*.*","../../../../nmglvs_mc12101-gcc/src_nmc0/*.*", "Makefile0" }
+	  libdirs { "$(NMPP)/lib","$(HAL)/lib", "../../../../lib"}
+	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../../../include"}
 	  links { "nmopengl-x86d.lib", "nmpp-x86.lib","hal-virtual-x86.lib"}
 	 
       configuration "Debug"
@@ -51,9 +51,9 @@ solution "demo3d-target1-x86"
    project "demo3d-target1-x86"
       kind "ConsoleApp"
 	  language "C++"
-      files { "../../include/*.h","../src_target1/*.*", "../../nmglvs_mc12101-gcc/src_nmc1/*.*", "Makefile1" }
-	  libdirs { "$(NMPP)/lib","$(HAL)/lib", "$(VSHELL)/lib", "../../lib"}
-	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include", "$(VSHELL)/include"}
+      files { "../../../../include/*.h","../src_target1/*.*", "../../../../nmglvs_mc12101-gcc/src_nmc1/*.*", "Makefile1" }
+	  libdirs { "$(NMPP)/lib","$(HAL)/lib", "$(VSHELL)/lib", "../../../../lib"}
+	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../../../include", "$(VSHELL)/include"}
 	  links { "nmopengl-x86d.lib", "nmpp-x86.lib","hal-virtual-x86.lib", "vshell.lib"}
 	 
       configuration "Debug"
