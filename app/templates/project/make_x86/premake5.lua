d0ba20a0975dabd408889626f46bfdfd91f4d073
Alex Klimenko <a.v.klimenko@mai.ru>
2020-08-03 17:14:27 +0300

    Исправил пример app/templates/project/make_x86
       До этого пример копилировался, но во время работы вылетала ошибка
    ошибка handshake с платой mc12101;
       Для исправления ошибки требовалось добавить в premake пути,
    присутствующие в примере 2nmc, и отсутствующие в искомом примере.

--- app/templates/project/make_x86/premake5.lua
+++ app/templates/project/make_x86/premake5.lua
@@ -10,14 +10,15 @@ solution "demo3d-host-x86"
 		files { "../../../../include/*.h","../src_host/*.cpp","../src_host/*.c", "../../../../nmglvs_mc12101-gcc/src_host/*.cpp"}
 		libdirs { "$(VSHELL)/lib","$(NMPP)/lib","$(HAL)/lib"}
 		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../../../include"}
-		links { "vshell.lib","nmpp-x86.lib","hal-virtual-x86.lib"}
+		links { "vshell.lib","nmpp-x86.lib","hal-virtual-x86d.lib"}
 
+      
       configuration "Debug"
-         defines { "DEBUG" }
+         defines { "DEBUG", "EMULATION" }
          symbols  "On" 
 
       configuration "Release"
-         defines { "NDEBUG" }
+         defines { "NDEBUG", "EMULATION" }
          symbols  "Off" 
 		 
 		 
@@ -30,7 +31,7 @@ solution "demo3d-target0-x86"
    project "demo3d-target0-x86"
       kind "ConsoleApp"
 	  language "C++"
-      files { "../../../../include/*.h","../src_target0/*.*","../../../../nmglvs_mc12101-gcc/src_nmc0/*.*", "Makefile0" }
+      files { "../../../../include/*.h","../src_target0/*.*","../../../../src_proc0/pc/*.*","../../../../src_proc0/common/*.*","../../../../src_proc0/nmgl/*.*","../../../../nmglvs_mc12101-gcc/src_nmc0/*.*", "Makefile0" }
 	  libdirs { "$(NMPP)/lib","$(HAL)/lib", "../../../../lib"}
 	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../../../include"}
 	  links { "nmopengl-x86d.lib", "nmpp-x86.lib","hal-virtual-x86.lib"}
@@ -51,7 +52,7 @@ solution "demo3d-target1-x86"
    project "demo3d-target1-x86"
       kind "ConsoleApp"
 	  language "C++"
-      files { "../../../../include/*.h","../src_target1/*.*", "../../../../nmglvs_mc12101-gcc/src_nmc1/*.*", "Makefile1" }
+      files { "../../../../include/*.h","../src_target1/*.*","../../../../src_proc1/pc/*.*","../../../../src_proc1/common/*.*", "../../../../nmglvs_mc12101-gcc/src_nmc1/*.*", "Makefile1" }
 	  libdirs { "$(NMPP)/lib","$(HAL)/lib", "$(VSHELL)/lib", "../../../../lib"}
 	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../../../include", "$(VSHELL)/include"}
 	  links { "nmopengl-x86d.lib", "nmpp-x86.lib","hal-virtual-x86.lib", "vshell.lib"}
