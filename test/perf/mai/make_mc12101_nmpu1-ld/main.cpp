afdadb5491b76107020a40a2a0c649e9637c0b4a
Pavel Zhdanov <p.a.zhdanov@mai.ru>
2020-08-11 11:47:10 +0300

    Fix main template: add headers, change ddr section name
    Main should contain service.h header.
    Change data_ddr to data_DDR just as in the linker script.
    Fix corresponding nm0/1 headers

--- test/perf/mai/make_mc12101_nmpu1-ld/main.cpp
+++ test/perf/mai/make_mc12101_nmpu1-ld/main.cpp
@@ -3,7 +3,8 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
-#include "demo3d_nm0.h"
+#include "demo3d_nm1.h"
+#include "service.h"
 
 __attribute__((section(".mem_bank0"))) long long im0[2048];
 __attribute__((section(".mem_bank1"))) long long im1[2048];
@@ -14,7 +15,7 @@ __attribute__((section(".mem_bank3"))) long long im3[2048];
 //__attribute__((section(".mem_bank6"))) long long im6[2048];
 //__attribute__((section(".mem_bank7"))) long long im7[2048];
 
-__attribute__((section(".data_ddr"))) long long emi[4096];
+__attribute__((section(".data_DDR"))) long long emi[4096];
 
 
 int main()
