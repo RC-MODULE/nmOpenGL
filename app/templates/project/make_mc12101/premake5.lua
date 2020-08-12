ecc0b4ab2fb0f85b691c5cf02a0e7a279bbc155c
Ivan Zhilenkov <i.zhilenkov@module.ru>
2020-08-12 14:55:07 +0300

    added x64

--- app/templates/project/make_mc12101/premake5.lua
+++ app/templates/project/make_mc12101/premake5.lua
@@ -3,26 +3,41 @@
 -- A solution contains projects, and defines the available configurations
 solution "demo3d-mc12101"
    configurations { "Debug", "Release" }
-   startproject "mc12101-x86"
+   startproject "mc12101-host"
+   platforms{"x86","x64"}
 
    -- A project defines one build target
-   project "mc12101-x86"
+   project "mc12101-host"
 		kind "ConsoleApp"
 		language "C++"
-		files { "../../include/*.h","../src_host/*.cpp", "../../nmglvs_mc12101-gcc/src_host/*.cpp"}
+		files { "../../include/*.h","../src_host/*.cpp","../src_host/*.c", "../../nmglvs_mc12101-gcc/src_host/*.cpp"}
 		libdirs { "$(MC12101)/lib","$(VSHELL)/lib","$(NMPP)/lib","$(HAL)/lib"}
 		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../include", "../include"}
-		links { "mc12101load.lib","vshell.lib","nmpp-x86.lib","hal-mc12101-x86d.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
-
-      configuration "Debug"
-         defines { "DEBUG" }
-         symbols  "On" 
-
-      configuration "Release"
-         defines { "NDEBUG" }
-         symbols  "Off" 
-		 
-		 
+		configuration {"Debug","x86"}
+			links { "mc12101load.lib","vshell.lib","nmpp-x86d.lib","hal-mc12101-x86d.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
+			targetsuffix ("-x86d")
+			architecture "x32"
+			defines { "DEBUG"}
+			symbols  "On" 
+		configuration {"Release","x86"}
+			links { "mc12101load.lib","vshell.lib","nmpp-x86.lib","hal-mc12101-x86.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
+			targetsuffix ("-x86")
+			architecture "x32"
+			defines { "NDEBUG"}
+			symbols  "Off" 
+		configuration {"Debug","x64"}
+			links { "mc12101load.lib","vshell.lib","nmpp-x64d.lib","hal-mc12101-x64d.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
+			targetsuffix ("-x64d")
+			architecture "x64"
+			defines { "DEBUG"}
+			symbols  "On" 
+		configuration {"Release","x64"}
+			links { "mc12101load.lib","vshell.lib","nmpp-x64.lib","hal-mc12101-x64.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
+			targetsuffix ("-x64")
+			architecture "x64"
+			defines { "NDEBUG"}
+			symbols  "Off" 
+	 
 	project "mc12101-nmc4-0"
       kind "Makefile"
       files { "../../include/*.h","../src_target0/*.*", "../../nmglvs_mc12101-gcc/src_nmc0/*.*", "mc12101brd-nmc0.cfg", "Makefile0" }
