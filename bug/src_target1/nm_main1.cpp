732082493e844143ec0c8e87efb4ccec0c196d1e
Ivan Zhilenkov <i.zhilenkov@module.ru>
2020-09-24 11:52:11 +0300

    triangulate bug

---
+++ bug/src_target1/nm_main1.cpp
@@ -0,0 +1,15 @@
+#include "demo3d_nm1.h"
+
+#include "nmgl.h"
+#include "nmglvs_nmc1.h"
+
+int main()
+{ 
+	nmglvsNm1Init();
+
+	while (nmglvsNm1Run()) {
+		nmglvsNm1Step();
+	}
+	return 0x611D611D;
+} 
+
