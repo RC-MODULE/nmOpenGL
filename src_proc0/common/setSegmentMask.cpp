948fc7e6c5d6adb00dc509179427e1a9f5edaf05
Ivan Zhilenkov <i.zhilenkov@module.ru>
2020-09-24 15:30:49 +0300

    windowsInfo++, optimized setSegmentMask

--- src_proc0/common/setSegmentMask.cpp
+++ src_proc0/common/setSegmentMask.cpp
@@ -4,34 +4,43 @@
 #include "nmblas.h"
 
 SECTION(".text_demo3d")
+void and4v(int* mask1, int* mask2, int* mask3, int* mask4, int* dst, int size) {
+	volatile int a= 0;
+	for (int i = 0, cnt = 0; cnt < size; i++, cnt += 32) {
+		int result = (mask1[i] & mask2[i]) & (mask3[i] & mask4[i]);
+		dst[i] = result;
+		a++;
+	}
+}
+
+SECTION(".text_demo3d")
 void setSegmentMask(v2nm32f* minXY, v2nm32f* maxXY, BitMask* masks, int size){
 	NMGL_Context_NM0 *cntxt = NMGL_Context_NM0::getContext();
-	for (int segY = 0, iSeg = 0; segY < cntxt->windowInfo.nRows; segY++) {
-		for (int segX = 0; segX < cntxt->windowInfo.nColumns; segX++, iSeg++) {
-			v2nm32f* lowerLimit = (v2nm32f*)&cntxt->tmp;
-			v2nm32f* upperLimit = (v2nm32f*)&cntxt->tmp.vec[2];
-			lowerLimit->v0 = cntxt->windowInfo.x0_f[segX];
-			lowerLimit->v1 = cntxt->windowInfo.y0_f[segY];
-			upperLimit->v0 = cntxt->windowInfo.x1_f[segX];
-			upperLimit->v1 = cntxt->windowInfo.y1_f[segY];
+	WindowInfo* winInfo = &cntxt->windowInfo;
+	int nRows = winInfo->nRows;
+	int nColumns = winInfo->nColumns;
+	v2nm32f* lowerLimit = winInfo->lowerLeft;
+	v2nm32f* upperLimit = winInfo->upperRight;
+
+	for (int segY = 0, iSeg = 0; segY < nRows; segY++) {
+		for (int segX = 0; segX < nColumns; segX++, iSeg++) {					
 
 			int* maskXLt = (int*)cntxt->buffer0;
 			int* maskYLt = (int*)cntxt->buffer1;
 			int* maskXGt = (int*)cntxt->buffer2;
 			int* maskYGt = (int*)cntxt->buffer3;
-			nmppsCmpLtC_v2nm32f((v2nm32f*)minXY, upperLimit, (nm1*)maskXLt, (nm1*)maskYLt, 1, size);
-			nmppsCmpGtC_v2nm32f((v2nm32f*)maxXY, lowerLimit, (nm1*)maskXGt, (nm1*)maskYGt, 1, size);
+
+			nmppsCmpLtC_v2nm32f((v2nm32f*)minXY, upperLimit + iSeg, (nm1*)maskXLt, (nm1*)maskYLt, 1, size);
+			nmppsCmpGtC_v2nm32f((v2nm32f*)maxXY, lowerLimit + iSeg, (nm1*)maskXGt, (nm1*)maskYGt, 1, size);
+			//and4v(maskXLt, maskYLt, maskXGt, maskYGt, masks[iSeg].bits, size);
 			for (int i = 0, cnt = 0; cnt < size; i++, cnt += 32) {
 				int result = (maskXLt[i] & maskYLt[i]) & (maskXGt[i] & maskYGt[i]);
 				masks[iSeg].bits[i] = result;
 			}
-			int size32 = MIN(NMGL_SIZE / 32, size / 32 + 2);
-			if (firstNonZeroIndx_32s(masks[iSeg].bits, size32) >= 0) {
-				masks[iSeg].hasNotZeroBits = 1;
-			}
-			else {
-				masks[iSeg].hasNotZeroBits = 0;
-			}
+			int size32 = (size + 31) / 32;
+			unsigned int ind = firstNonZeroIndx_32s(masks[iSeg].bits, size32);
+			//если ind = -1, значит все нули и в hasNotZeroBits писать 1, иначе наоборот
+			masks[iSeg].hasNotZeroBits = ~ind >> 31;
 		}
 	}
 }
