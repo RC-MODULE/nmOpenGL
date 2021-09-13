274ed08309e8b1af222701d4c77a620379060638
Александр Жданов <a.a.zhdanov@mai.ru>
2021-09-13 11:40:59 +0300

    Filter func refactoring. Move shiftedZeroes table to separate file.
    shiftedZeroes table (32 words) is used in each of 12 cmp.._f filter
    functions. To save memory shiftedZeroes array was moved to separate
    asm file

--- src_proc0/nm/cmpGteC_f.asm
+++ src_proc0/nm/cmpGteC_f.asm
@@ -1,49 +1,6 @@
+extern shiftedZeroes: word;
 data ".data_demo3d"
-  shiftedZeroes: word[32] = ( 
-		0fffffffeh,
-		0fffffffdh,
-		0fffffffbh,
-		0fffffff7h,
-
-		0ffffffefh,
-		0ffffffdfh,
-		0ffffffbfh,
-		0ffffff7fh,
-
-		0fffffeffh,
-		0fffffdffh,
-		0fffffbffh,
-		0fffff7ffh,
-
-		0ffffefffh,
-		0ffffdfffh,
-		0ffffbfffh,
-		0ffff7fffh,
-
-		0fffeffffh,
-		0fffdffffh,
-		0fffbffffh,
-		0fff7ffffh,
-
-		0ffefffffh,
-		0ffdfffffh,
-		0ffbfffffh,
-		0ff7fffffh,
-
-		0feffffffh,
-		0fdffffffh,
-		0fbffffffh,
-		0f7ffffffh,
-
-		0efffffffh,
-		0dfffffffh,
-		0bfffffffh,
-		07fffffffh
-
-	);
-
   dupC: long = 0hl;
-
 end ".data_demo3d";
 
 //void cmpGteC_f(float* src, float C, nm1* dstMaskEven, nm1* dstMaskOdd, int size);
