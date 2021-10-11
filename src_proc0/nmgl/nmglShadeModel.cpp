7fb650d1cfc6e7c947a54245021f7315519ee9e2
Александр Жданов <a.a.zhdanov@mai.ru>
2021-10-11 11:52:28 +0300

    Fix nmglShadeModel api function
    There was an error in function: it didn't set shadeModel
    field in nm0 context, only in nm1 context.

--- src_proc0/nmgl/nmglShadeModel.cpp
+++ src_proc0/nmgl/nmglShadeModel.cpp
@@ -9,6 +9,9 @@ SECTION(".text_nmgl")
 void nmglShadeModel (NMGLenum mode){
 	NMGL_Context_NM0 *cntxt = NMGL_Context_NM0::getContext();
 	if ((mode == NMGL_FLAT) || (mode == NMGL_SMOOTH)) {
+
+		cntxt->shadeModel = mode;	
+
 		NM_Command command;
 		command.instr = NMC1_SHADE_MODEL;
 		command.params[0] = CommandArgument(mode);
