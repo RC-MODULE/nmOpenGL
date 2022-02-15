3b2eea8096423131c86d214505d955c892705552
Pavel Zhdanov <p.a.zhdanov@mai.ru>
2022-02-15 14:55:09 +0300

    Use absolute and relative epsilon for float comparison, add new macros
    * Use combined approach to compare floats (absolute and relative epsilons).
    Relative epsilon adapts to the numbers compared. An absolute epsilon gives true
    when two float numbers are very close to each other.
    (https://embeddeduse.com/2019/08/26/qt-compare-two-floats/)
    
    * Modify macros used for float comparisons. There are two versions now: with and
    without the precision (the second version uses FLT_EPSILON as an epsilon)
    
    * Add new macros to compare floats.

--- include/tests.h
+++ include/tests.h
@@ -5,7 +5,7 @@
 #include <math.h>
 
 /* Compare two floats, 1 if are equal, 0 - otherwise */
-static int equalf(float a, float b)
+static int equalf(float a, float b, float epsilon)
 {
 	if (((a < 0) && (b>=0)) || ((a >= 0) && (b < 0)))
 		return 0;
@@ -14,11 +14,19 @@ static int equalf(float a, float b)
 	b = fabs(b);
 
 	float max = (a > b)? a: b;
-	if ( fabs(a-b) <= (FLT_EPSILON * max)){
+
+	if ((fabs(a - b) <= (epsilon * max))	||
+		(fabs(a - b) <= epsilon)){
 		return 1;
 	} else {
+		puts("\n\n");
+		printf("fabs(a - b)(%15.12f) <= (epsilon * max)(%15.12f): %i\n", 
+				fabs(a - b), epsilon * max, fabs(a-b) <= (epsilon * max));
+		printf("fabs(a - b)(%15.12f) <= epsilon(%15.12f): %i\n", 
+				fabs(a - b), epsilon, fabs(a-b) <= epsilon);
 		return 0;
 	}
+	
 }
 
 #define TEST_ASSERT(x, ...) \
@@ -30,16 +38,30 @@ if (!(x)) \
 	return -1;\
 }
 
+#define TEST_ASSERT_EQUALF_PRECISION(real, expected, epsilon, ...) \
+{ \
+	if (!equalf(real, expected, epsilon)){ \
+		printf ("\nFAIL Function: %s\n     File:%s\n     Line:%d\n     Cond:%s\n",__FUNCTION__, __FILE__, __LINE__, "real is not equal to expected"); \
+		printf ("     Details: " __VA_ARGS__);\
+		printf ("\n\n");\
+		return -1; \
+	} else { \
+		/* Do nothing, continue to the next element */ \
+	} \
+}
+
+#define TEST_ASSERT_EQUALF(real, expected, ...) \
+	TEST_ASSERT_EQUALF_PRECISION(real, expected, FLT_EPSILON, __VA_ARGS__)
+
 #define TEST_ASSERT_ACCUM(x) \
 if (!(x)) \
 	_accum++;\
 
-
-#define TEST_ARRAYS_EQUAL(array1, array2, size, ...) \
+#define TEST_ARRAYS_EQUAL_PRECISION(array1, array2, size, epsilon, ...) \
 { \
 	int __i; \
 	for (__i = 0; __i < size; ++__i){ \
-		if (!equalf(array1[__i], array2[__i])) { \
+		if (!equalf(array1[__i], array2[__i], epsilon)) { \
 			printf ("\nFAIL Function: %s\n     File:%s\n     Line:%d\n     Cond:%s\n",__FUNCTION__, __FILE__, __LINE__, "all elements of arrays are equal"); \
 			printf ("     Details: " __VA_ARGS__);\
 			printf ("\n\n");\
@@ -50,12 +72,15 @@ if (!(x)) \
 	} \
 }
 
-#define TEST_ARRAYS_NOT_EQUAL(array1, array2, size, ...) \
+#define TEST_ARRAYS_EQUAL(array1, array2, size, ...) \
+	TEST_ARRAYS_EQUAL_PRECISION(array1, array2, size, FLT_EPSILON, __VA_ARGS__)
+
+#define TEST_ARRAYS_NOT_EQUAL_PRECISION(array1, array2, size, epsilon, ...) \
 { \
 	int __i; \
 	int __res = -1; \
 	for (__i = 0; __i < size; ++__i){ \
-		if (!equalf(array1[__i], array2[__i])) { \
+		if (!equalf(array1[__i], array2[__i], epsilon)) { \
 			__res = 0;\
 			break;\
 		}\
@@ -69,6 +94,9 @@ if (!(x)) \
 		/* Do nothing, continue to the next element */ \
 	} \
 }
+
+#define TEST_ARRAYS_NOT_EQUAL(array1, array2, size, ...) \
+	TEST_ARRAYS_NOT_EQUAL_PRECISION(array1, array2, size, FLT_EPSILON, __VA_ARGS__)
 
 // Compare if two integer arrays are equal
 #define TEST_ARRAYS_EQUALI(array1, array2, size, ...) \
@@ -85,14 +113,15 @@ if (!(x)) \
 		} \
 	} \
 }
-#define TEST_VEC_ARRAYS_EQUAL(array1, array2, size, ...) \
+
+#define TEST_VEC_ARRAYS_EQUAL_PRECISION(array1, array2, size, epsilon, ...) \
 { \
 	volatile int __i; \
 	for (__i = 0; __i < size; ++__i){ \
-		if ((!equalf(array1[__i].vec[0], array2[__i].vec[0])) || \
-			(!equalf(array1[__i].vec[1], array2[__i].vec[1])) || \
-			(!equalf(array1[__i].vec[2], array2[__i].vec[2])) || \
-			(!equalf(array1[__i].vec[3], array2[__i].vec[3]))) { \
+		if ((!equalf(array1[__i].vec[0], array2[__i].vec[0], epsilon)) || \
+			(!equalf(array1[__i].vec[1], array2[__i].vec[1], epsilon)) || \
+			(!equalf(array1[__i].vec[2], array2[__i].vec[2], epsilon)) || \
+			(!equalf(array1[__i].vec[3], array2[__i].vec[3], epsilon))) { \
 			printf ("\nFAIL Function: %s\n     File:%s\n     Line:%d\n     Cond:%s\n",__FUNCTION__, __FILE__, __LINE__, "all elements of arrays are equal"); \
 			printf ("     Details: " __VA_ARGS__);\
 			printf ("\n\n");\
@@ -102,6 +131,9 @@ if (!(x)) \
 		} \
 	} \
 }
+
+#define TEST_VEC_ARRAYS_EQUAL(array1, array2, size, ...) \
+	TEST_VEC_ARRAYS_EQUAL_PRECISION(array1, array2, size, FLT_EPSILON, __VA_ARGS__)
 
 // Compares arrays of struct {unsigned long long vec[1]}
 // Implemented to compare RGB8888 arrays
@@ -129,6 +161,13 @@ if (!(x)) \
 
 //Macro to run test. usage: RUN_TEST(functionMane_condition_result)
 #define RUN_TEST(x) if(x() == 0) printf ("OK   Function: %s\n", #x);
+
+//Macro to run performance test. usage: RUN_PERF_TEST(functionMane_condition_result)
+#define RUN_PERF_TEST(x) \
+{\
+	printf ("\nOK   Function: %s\n", #x);\
+	x();\
+}
 
 //Macro to run test with arguments. usage: RUN_ARG_TEST(functionMane_condition_result,arg)
 #define RUN_ARG_TEST(f, arg) \
