bd57dc3d5be7256888bb7f57756abc2ecc0c34ab
Александр Жданов <a.a.zhdanov@mai.ru>
2022-02-15 16:10:43 +0300

    Move unit tests for filter functions to utilities
    * Filter functions were moved to utilities, so unit test also were moved
      to utilities/test. Test for cnvMaskToIndicies was replaced to test
      from test/nmpu0/cnvMaskToIndices.
    * Unit tests for filter functions use tests.h, so path to tests.h was
      added to Sconscript for common and float utility functions tests.
    * #include 'demo3d_common.h' were removed from tests because it is not
      used.

--- utilities/tests/common/cnvMaskToOffsets/main.cpp
+++ utilities/tests/common/cnvMaskToOffsets/main.cpp
@@ -5,29 +5,28 @@
 
 #define		TEST_MASK_SIZE 		17	
 
-int cnvMaskToIndices_allOnes_returns17();
-int cnvMaskToIndices_allZeroes_returns0();
-int cnvMaskToIndices_randomOnes_returns9();
-int cnvMaskToIndices_size0_resultCorrect();
-int cnvMaskToIndices_size1_resultCorrect();
-int cnvMaskToIndices_size32_resultCorrect();
-int cnvMaskToIndices_size33_resultCorrect();
-int cnvMaskToIndices_size33_dstVecExtraElementsNotChanged();
+int cnvMaskToOffsets_allOnes_returns17();
+int cnvMaskToOffsets_allZeroes_returns0();
+int cnvMaskToOffsets_randomOnes_returns9();
+int cnvMaskToOffsets_size0_resultCorrect();
+int cnvMaskToOffsets_size1_resultCorrect();
+int cnvMaskToOffsets_size32_resultCorrect();
+int cnvMaskToOffsets_size33_resultCorrect();
+int cnvMaskToOffsets_size33_dstVecExtraElementsNotChanged();
 
-extern "C" int cnvMaskToIndices(nm1* mask, int* indices, int size);
+extern "C" int cnvMaskToOffsets(nm1* mask, int* offsets, int size);
 nm32s arrayToInt (unsigned int* array, int count);
-
 
 int main()
 {
-  RUN_TEST(cnvMaskToIndices_allOnes_returns17);
-  RUN_TEST(cnvMaskToIndices_allZeroes_returns0);
-  RUN_TEST(cnvMaskToIndices_randomOnes_returns9);
-  RUN_TEST(cnvMaskToIndices_size0_resultCorrect);
-  RUN_TEST(cnvMaskToIndices_size1_resultCorrect);
-  RUN_TEST(cnvMaskToIndices_size32_resultCorrect);
-  RUN_TEST(cnvMaskToIndices_size33_resultCorrect);
-  RUN_TEST(cnvMaskToIndices_size33_dstVecExtraElementsNotChanged);
+  RUN_TEST(cnvMaskToOffsets_allOnes_returns17);
+  RUN_TEST(cnvMaskToOffsets_allZeroes_returns0);
+  RUN_TEST(cnvMaskToOffsets_randomOnes_returns9);
+  RUN_TEST(cnvMaskToOffsets_size0_resultCorrect);
+  RUN_TEST(cnvMaskToOffsets_size1_resultCorrect);
+  RUN_TEST(cnvMaskToOffsets_size32_resultCorrect);
+  RUN_TEST(cnvMaskToOffsets_size33_resultCorrect);
+  RUN_TEST(cnvMaskToOffsets_size33_dstVecExtraElementsNotChanged);
 
 	return 0;
 }
@@ -42,17 +41,16 @@ nm32s arrayToInt (unsigned int* array, int count)
   return res;
 }
 
-
-int cnvMaskToIndices_allOnes_returns17(){
+int cnvMaskToOffsets_allOnes_returns17(){
 
   unsigned int testMaskArray[TEST_MASK_SIZE] = {1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1};
   constexpr int size = TEST_MASK_SIZE;
   int dstVec[size];
+  int refDstVec[size]               = {0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1, 1};
   int refResult = 17; 
-  int refDstVec[size]   = {0,1,2,3, 4,5,6,7, 8,9,10,11, 12,13,14,15, 16};
 
   nm32s testMask = arrayToInt (testMaskArray, TEST_MASK_SIZE);
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   printf("testMask = %0x\n", testMask);
@@ -76,13 +74,13 @@ int cnvMaskToIndices_allOnes_returns17(){
   return 0;
 }
 
-int cnvMaskToIndices_allZeroes_returns0(){
+int cnvMaskToOffsets_allZeroes_returns0(){
 
   unsigned int testMaskArray[TEST_MASK_SIZE] = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0};
   constexpr int size = TEST_MASK_SIZE;
   int dstVec[size];
   int refDstVec[size];
-  int refResult = 0; 
+  int refResult  = 0; 
 
   for (int i = 0; i < size; i++) {
     dstVec[i] = i;
@@ -90,7 +88,7 @@ int cnvMaskToIndices_allZeroes_returns0(){
   }
 
   nm32s testMask = arrayToInt (testMaskArray, TEST_MASK_SIZE);
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   printf("testMask = %0x\n", testMask);
@@ -114,17 +112,16 @@ int cnvMaskToIndices_allZeroes_returns0(){
   return 0;
 }
 
-
-int cnvMaskToIndices_randomOnes_returns9(){
+int cnvMaskToOffsets_randomOnes_returns9(){
 
   unsigned int testMaskArray[TEST_MASK_SIZE] = {0,0,1,1, 0,1,0,1, 0,1,0,1, 1,1,0,1, 0};
   constexpr int size = TEST_MASK_SIZE;
   int dstVec[size];
-  int refDstVec[size] = {2,3,5,7,9,11,12,13,15};
+  int refDstVec[size]               = {    2,1,   2,  2,   2,  2, 1,1,  2};
   int refResult = 9; 
 
   nm32s testMask = arrayToInt (testMaskArray, TEST_MASK_SIZE);
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   printf("testMask = %0x\n", testMask);
@@ -148,7 +145,7 @@ int cnvMaskToIndices_randomOnes_returns9(){
   return 0;
 }
 
-int cnvMaskToIndices_size0_resultCorrect(){
+int cnvMaskToOffsets_size0_resultCorrect(){
 
   constexpr int maxResult = 32;
   int dstVec[maxResult] = {0};
@@ -162,7 +159,7 @@ int cnvMaskToIndices_size0_resultCorrect(){
     refDstVec[i] = dstVec[i];
   }
 
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   printf("testMask = %0x\n", testMask);
@@ -186,7 +183,7 @@ int cnvMaskToIndices_size0_resultCorrect(){
   return 0;
 }
 
-int cnvMaskToIndices_size1_resultCorrect(){
+int cnvMaskToOffsets_size1_resultCorrect(){
 
   constexpr int maxResult = 32;
   int dstVec[maxResult] = {777,777,777}; //some not null values at the beginning
@@ -195,7 +192,7 @@ int cnvMaskToIndices_size1_resultCorrect(){
   int refResult = 1; 
   int refDstVec[maxResult] = {0,777,777};
 
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   printf("testMask = %0x\n", testMask);
@@ -219,52 +216,16 @@ int cnvMaskToIndices_size1_resultCorrect(){
   return 0;
 }
 
-int cnvMaskToIndices_size32_resultCorrect(){
+int cnvMaskToOffsets_size32_resultCorrect(){
 
   constexpr int maxResult = 64;
   int dstVec[maxResult] = {0}; 
   unsigned int testMask[2] = {0xc0000000, 0xffffffff};
   int size = 32;
   int refResult = 2; 
-  int refDstVec[maxResult] = {30,31};
+  int refDstVec[maxResult] = {30,1};
 
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
-
-#ifdef DEBUG
-  for (int i = 0; i < 2; i++){
-    printf("testMask[%d] = %0x\n", i, testMask[i]);
-  }
-  printf("size= %d\n", size);
-  printf("result = %d\n", result);
-  printf("dstVec: ");
-  for (int i = 0; i < maxResult; i++){
-    printf("%3d ", dstVec[i]);
-  }
-  printf("\r\n");
-
-  printf("refVec: ");
-  for (int i = 0; i < maxResult; i++){
-    printf("%3d ", refDstVec[i]);
-  }
-  printf("\r\n");
-#endif //DEBUG
-
-  TEST_ASSERT(result == refResult);
-  TEST_ARRAYS_EQUALI(dstVec, refDstVec, maxResult);
-
-  return 0;
-}
-
-int cnvMaskToIndices_size33_resultCorrect(){
-
-  constexpr int maxResult = 64;
-  int dstVec[maxResult] = {0}; 
-  unsigned int testMask[2] = {0xc0000000, 0xffffffff};
-  int size = 33;
-  int refResult = 3; 
-  int refDstVec[maxResult] = {30,31,32};
-
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   for (int i = 0; i < 2; i++){
@@ -291,20 +252,55 @@ int cnvMaskToIndices_size33_resultCorrect(){
   return 0;
 }
 
-
-int cnvMaskToIndices_size33_dstVecExtraElementsNotChanged(){
+int cnvMaskToOffsets_size33_resultCorrect(){
 
   constexpr int maxResult = 64;
   int dstVec[maxResult] = {0}; 
   unsigned int testMask[2] = {0xc0000000, 0xffffffff};
   int size = 33;
   int refResult = 3; 
-  int refDstVec[maxResult] = {30,31,32};
+  int refDstVec[maxResult] = {30,1,1};
+
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
+
+#ifdef DEBUG
+  for (int i = 0; i < 2; i++){
+    printf("testMask[%d] = %0x\n", i, testMask[i]);
+  }
+  printf("size= %d\n", size);
+  printf("result = %d\n", result);
+  printf("dstVec: ");
+  for (int i = 0; i < maxResult; i++){
+    printf("%3d ", dstVec[i]);
+  }
+  printf("\r\n");
+
+  printf("refVec: ");
+  for (int i = 0; i < maxResult; i++){
+    printf("%3d ", refDstVec[i]);
+  }
+  printf("\r\n");
+#endif //DEBUG
+
+  TEST_ASSERT(result == refResult);
+  TEST_ARRAYS_EQUALI(dstVec, refDstVec, maxResult);
+
+  return 0;
+}
+
+int cnvMaskToOffsets_size33_dstVecExtraElementsNotChanged(){
+
+  constexpr int maxResult = 64;
+  int dstVec[maxResult] = {0}; 
+  unsigned int testMask[2] = {0xc0000000, 0xffffffff};
+  int size = 33;
+  int refResult = 3; 
+  int refDstVec[maxResult] = {30,1,1};
 
   dstVec[3] = refDstVec[3] = 777;
   dstVec[4] = refDstVec[4] = 888;
 
-  int result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  int result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   for (int i = 0; i < 2; i++){
@@ -335,7 +331,7 @@ int cnvMaskToIndices_size33_dstVecExtraElementsNotChanged(){
   dstVec[3] = refDstVec[3] = refDstVec[6];
   dstVec[4] = refDstVec[4] = tmp;
 
-  result = cnvMaskToIndices((nm1*)(&testMask), dstVec, size);
+  result = cnvMaskToOffsets((nm1*)(&testMask), dstVec, size);
 
 #ifdef DEBUG
   for (int i = 0; i < 2; i++){
@@ -358,6 +354,6 @@ int cnvMaskToIndices_size33_dstVecExtraElementsNotChanged(){
 
   TEST_ASSERT(result == refResult);
   TEST_ARRAYS_EQUALI(dstVec, refDstVec, maxResult);
+
   return 0;
 }
-
---
+++ utilities/tests/common/deselectElementsByIndices/main.cpp
@@ -0,0 +1,174 @@
+#include "nmpp.h"
+#include "time.h"
+#include <stdio.h>
+#include "tests.h"
+
+#define		FLOAT_ARRAY_SRC_SIZE 3	
+#define		FLOAT_ARRAY_DST_SIZE 9	
+
+#define		INT_ARRAY_SRC_SIZE 3	
+#define		INT_ARRAY_DST_SIZE 9	
+
+#define		OBJECT_ARRAY_SRC_SIZE 3	
+#define		OBJECT_ARRAY_DST_SIZE 64	
+
+int deselectElementsByIndices_deselectFloats();
+int deselectElementsByIndices_deselectInts();
+int deselectElementsByIndices_deselectObjects();
+
+class testClass {
+public:
+  int a;
+  char b;
+  float c;
+  long d;
+  testClass(int a = 1, char b = 'a', float c = 1.0f, long d = -1):a(a), b(b), c(c), d(d){}
+  void print();
+  friend bool operator!= (const testClass& c1, const testClass& c2);
+ };
+
+void testClass::print () {
+ printf ("%d %c %f %ld\n", a, b, c, d);
+}
+
+bool operator!= (const testClass& c1, const testClass& c2){
+  return (c1.a != c2.a ||
+          c1.b != c2.b ||
+          c1.c != c2.c ||
+          c1.d != c2.d);
+}
+
+extern "C" void deselectElementsByIndices(void* src, int* indices, void* dst, int sizeOfElementInt, int size);
+
+int main()
+{
+
+  RUN_TEST(deselectElementsByIndices_deselectFloats);
+  RUN_TEST(deselectElementsByIndices_deselectInts);
+  RUN_TEST(deselectElementsByIndices_deselectObjects);
+
+	return 0;
+}
+
+
+int deselectElementsByIndices_deselectFloats() {
+
+  float src[FLOAT_ARRAY_SRC_SIZE] = {1.5,3.5,2.5};
+  int indices[3]={1,3,2};
+  int size = 3;
+  float dst[FLOAT_ARRAY_DST_SIZE]   ={800.5, 0.0, 0.0, 0.0, 274.5, 721.5, 289.5, 777.5, 118.5};
+  float dstRef[FLOAT_ARRAY_DST_SIZE]={800.5, 1.5, 2.5, 3.5, 274.5, 721.5, 289.5, 777.5, 118.5};
+
+  deselectElementsByIndices(src, indices, dst, sizeof32(int), size);
+
+#ifdef DEBUG
+  for(int i=0; i < FLOAT_ARRAY_DST_SIZE; i++){
+    printf("dst[%d]=%d\n", i, dst[i]);
+  }
+  printf(".\n");
+
+  for(int i=0; i < FLOAT_ARRAY_DST_SIZE; i++){
+    printf("dstRef[%d]=%d\n", i, dstRef[i]);
+  }
+  printf(".\n");
+#endif //DEBUG
+
+  TEST_ARRAYS_EQUAL(dst, dstRef, size);
+
+  return 0;
+}
+
+int deselectElementsByIndices_deselectInts() {
+
+  int src[INT_ARRAY_SRC_SIZE]={777, 888, 777};
+  int indices[INT_ARRAY_SRC_SIZE + 1]={2,3,2,1};
+  int size = 3;
+  int dst[INT_ARRAY_DST_SIZE]   ={0xC0DE0001, 0xC0DE0002, 999, 999, 736, 884, 997, 457, 900};
+  int dstRef[INT_ARRAY_DST_SIZE]={0xC0DE0001, 0xC0DE0002, 777, 888, 736, 884, 997, 457, 900};
+
+  deselectElementsByIndices(src, indices, dst, sizeof32(int), size);
+
+#ifdef DEBUG
+  for(int i=0; i < INT_ARRAY_DST_SIZE; i++){
+    printf("dst[%d]=%d\n", i, dst[i]);
+  }
+  printf(".\n");
+
+  for(int i=0; i < INT_ARRAY_DST_SIZE; i++){
+    printf("dstRef[%d]=%d\n", i, dstRef[i]);
+  }
+  printf(".\n");
+#endif //DEBUG
+
+  TEST_ARRAYS_EQUALI(dst, dstRef, size);
+
+  return 0;
+
+}
+
+
+int deselectElementsByIndices_deselectObjects() {
+
+  testClass src[OBJECT_ARRAY_SRC_SIZE];
+  int indices[3] = {0,1,2};
+  int size = 3;
+  testClass dst[OBJECT_ARRAY_DST_SIZE];
+  testClass dstRef[OBJECT_ARRAY_DST_SIZE];
+
+  src[0] = testClass(1,'a',1.5,100);
+  src[1] = testClass(2,'b',2.5,200);
+  src[2] = testClass(3,'c',3.5,300);
+
+  for (int i = 0; i < OBJECT_ARRAY_DST_SIZE; i++) {
+    dst[i] = testClass(0,'z',0.5,777);
+  }
+
+  for (int i = 0; i < OBJECT_ARRAY_DST_SIZE; i++) {
+    dstRef[i] = dst[i];
+  }
+  dstRef[indices[0]] = src[0];
+  dstRef[indices[1]] = src[1];
+  dstRef[indices[2]] = src[2];
+
+  deselectElementsByIndices(src, indices, dst, sizeof32(testClass), size);
+
+#ifdef DEBUG
+  for(int i=0; i < OBJECT_ARRAY_DST_SIZE; i++){
+    printf("dst[%d]=", i);
+    dst[i].print();
+  }
+  printf(".\n");
+
+  for(int i=0; i < OBJECT_ARRAY_DST_SIZE; i++){
+    printf("dstRef[%d]=", i);
+    dstRef[i].print();
+  }
+#endif //DEBUG
+
+  TEST_ARRAYS_EQUALI(dst, dstRef, OBJECT_ARRAY_DST_SIZE);
+
+  //test one more time
+  src[0] = testClass(63,'f',63.5,6300);
+  indices[0] = OBJECT_ARRAY_DST_SIZE-1; //copy to last element of dst
+  size = 1;
+  dstRef[indices[0]] = src[0];
+
+  deselectElementsByIndices(src, indices, dst, sizeof32(testClass), size);
+
+#ifdef DEBUG
+  for(int i=0; i < OBJECT_ARRAY_DST_SIZE; i++){
+    printf("dst[%d]=", i);
+    dst[i].print();
+  }
+  printf(".\n");
+
+  for(int i=0; i < OBJECT_ARRAY_DST_SIZE; i++){
+    printf("dstRef[%d]=", i);
+    dstRef[i].print();
+  }
+#endif //DEBUG
+
+  TEST_ARRAYS_EQUALI(dst, dstRef, OBJECT_ARRAY_DST_SIZE);
+
+  return 0;
+}
