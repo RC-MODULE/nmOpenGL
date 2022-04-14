6d51fc46339e62c382b5644f94f4a030c53a5b46
Ivan Zhilenkov <i.zhilenkov@module.ru>
2022-04-14 14:37:09 +0300

    Makefiles++

--- texture-demo-gcc/make_mc12101/premake5.lua
+++ texture-demo-gcc/make_mc12101/premake5.lua
@@ -12,7 +12,7 @@ solution "demo3d-mc12101"
 		language "C++"
 		files { "../../include/*.h","../src_host/*.cpp","../src_host/*.c", "../../nmglvs_mc12101-gcc/src_host/*.cpp"}
 		libdirs { "$(MC12101)/lib","$(VSHELL)/lib","$(NMPP)/lib","$(HAL)/lib"}
-		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../include", "../include"}
+		includedirs { "$(MC12101)/include","$(HAL)/include","$(VSHELL)/include","$(NMPP)/include","../../include", "../../utilities/include", "../../context/include"}
 		configuration {"Debug","x86"}
 			links { "mc12101load.lib","vshell.lib","nmpp-x86d.lib","hal-mc12101-x86d.lib","mc12101-nmc4-0","mc12101-nmc4-1"}
 			targetsuffix ("-x86d")
@@ -41,37 +41,37 @@ solution "demo3d-mc12101"
 	project "mc12101-nmc4-0"
       kind "Makefile"
       files { "../../include/*.h","../src_target0/*.*", "../../nmglvs_mc12101-gcc/src_nmc0/*.*", "mc12101brd-nmc0.cfg", "Makefile0" }
-	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include","../include"}
+	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include", "../../utilities/include", "../include"}
 	  defines { "__GNUC__"}
 	 
 	  configuration "Debug"
 		   defines { "DEBUG"}
-		   buildcommands {"make DEBUG=y -f Makefile0 -j4"}
-		   rebuildcommands {"make -B DEBUG=y -f Makefile0  -j4"}
+		   buildcommands {"make nm0"}
+		   rebuildcommands {"make -B nm0"}
 		   cleancommands {"make clean"}
 		   
 	  configuration "Release"
 		   defines { "NDEBUG"}
-		   buildcommands {"make -f Makefile0 -j4"}
-		   rebuildcommands {"make -B -f Makefile0 -j4"}
+		   buildcommands {"make nm0"}
+		   rebuildcommands {"make -B nm0"}
 		   cleancommands {"make clean"}		   
 		   
 	project "mc12101-nmc4-1"
       kind "Makefile"
       files { "../../include/*.h","../src_target1/*.*", "../../nmglvs_mc12101-gcc/src_nmc1/*.*","mc12101brd-nmc1.cfg", "Makefile1" }
-	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include"}
+	  includedirs { "$(MC12101)/include","$(HAL)/include","$(NMPP)/include","../../include", "../../utilities/include"}
 	  defines { "__GNUC__"}
 	 
 	  configuration "Debug"
 		   defines { "DEBUG"}
-		   buildcommands {"make DEBUG=y -f Makefile1 -j4"}
-		   rebuildcommands {"make -B DEBUG=y -f Makefile1  -j4"}
+		   buildcommands {"make nm1"}
+		   rebuildcommands {"make -B nm1"}
 		   cleancommands {"make clean"}
 		   
 	  configuration "Release"
 		   defines { "NDEBUG"}
-		   buildcommands {"make -f Makefile1  -j4"}
-		   rebuildcommands {"make -B -f Makefile1  -j4"}
+		   buildcommands {"make nm1"}
+		   rebuildcommands {"make -B nm1"}
 		   cleancommands {"make clean"}		   
 		   
 		  
